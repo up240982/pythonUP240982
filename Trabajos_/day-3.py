534f6a06b48632be5402e9e014b6042cdb5c71d9
UP240982 <up240982@alumnos.>
2025-03-04 08:05:54 -0600

    test 2

---
+++ Trabajos_/day-3.py
@@ -0,0 +1,27 @@
+# Arithmetic Operations in Python
+# Integers
+
+# Ejercicio 1, Declarar tu edad como una variable entera 
+edad = 20 
+print("tu edad es: ", edad )
+#esta variable es del tipo entera por que no tiene decimales
+
+# Ejercicio 2, Declara tu estatura como una variable tipo flotante
+estatura = 1.78
+print("tu estatura es: ", estatura)
+#esta variable es de tipo flotante porque tiene decimales
+
+#Ejercicio 3, Declara una variable con un numero complejo
+numerocomplejo = 7j 
+print("tu numero complejo es: ", numerocomplejo)
+#esta variable es un numero complejo por la letra j
+
+#Calculaer el area de un traingulo 
+print("Calcular el area de un traingulo")
+base = int(input("Intrdocuce la base del triangulo: "))
+altura = int(input("Intrdocuce la altura del triangulo: "))
+
+area = ( 0.5 * base * altura )
+
+print("El resultado es:", area )
+
