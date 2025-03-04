410d37fec2adced77d37a398e33b78bc1a3a7b66
UP240982 <up240982@alumnos.>
2025-03-04 08:46:54 -0600

    .

--- Trabajos_/day-3.py
+++ Trabajos_/day-3.py
@@ -1,24 +1,32 @@
 # Arithmetic Operations in Python
 # Integers
 
-# Ejercicio 1, Declarar tu edad como una variable entera 
+# Ejercicio 1, Declarar tu edad como una variable entera .
 edad = 20 
 print("tu edad es: ", edad )
-#esta variable es del tipo entera por que no tiene decimales
+#esta variable es de tipo entera por que no contiene decimales.
 
 # Ejercicio 2, Declara tu estatura como una variable tipo flotante
 estatura = 1.78
 print("tu estatura es: ", estatura)
-#esta variable es de tipo flotante porque tiene decimales
+#esta variable es de tipo flotante porque contiene decimales.
 
-#Ejercicio 3, Declara una variable con un numero complejo
+#Ejercicio 3, Declara una variable con un numero complejo.
 numerocomplejo = 7j 
 print("tu numero complejo es: ", numerocomplejo)
-#esta variable es un numero complejo por la letra j
+#esta variable es un numero complejo por la letra j.
 
-#Calculaer el area de un traingulo 
+#Ejercicio 4, Calcular el area de un traingulo.
 print("Calcular el area de un traingulo")
 base = int(input("Intrdocuce la base del triangulo: "))
 altura = int(input("Intrdocuce la altura del triangulo: "))
 area = ( 0.5 * base * altura )
 print("El resultado es:", area )
+
+#Ejercicio 5, Cacular el perimetro de un triangulo.
+print("Calcular el perimetro de un triangulo")
+ladoA = int(input("Introduce el lado a de un triangulo: "))
+ladoB = int(input("Introduce el lado b de un triangulo: "))
+ladoC = int(input("Introduce el lado c de un triangulo: "))
+perimetro = (ladoA + ladoB + ladoC)
+print("El resultado es:", perimetro)
