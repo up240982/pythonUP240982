77cf48ec41dfa7805b57ea7cb1f1da2e93224682
UP240982 <up240982@alumnos.>
2025-04-03 20:32:01 -0600

    terminado dia 15, ya pasame co n 10!

--- 15_Day/day-15.py
+++ 15_Day/day-15.py
@@ -5,7 +5,7 @@ print("Ejercicios de Nivel 1:")
  print("Ejemplos de errores:")
  
 # Ejemplo 1, Syntaxerror
- print('Hoy gana el America'
+ print('Paseme con 10, tire paro'
        #SyntaxError: No se ejecuta el print porque no se cierra el parentesis.
  
 # Ejemplo 2, NameError     
@@ -22,12 +22,12 @@ print("Ejercicios de Nivel 1:")
      #ModuleNotFoundError: No se ejecuta el import porque no existe el modulo errupstorpedo.
 
 # Ejemplo 5, AttributeError
-  import date
-  date.ends
-     #AttributeError: No se ejecuta el import porque no existe el modulo date.
+  import range
+  range.add
+     #AttributeError: No se ejecuta el import porque no existe el modulo range.
 
 # Ejemplo 6, KeyError:
-  info = {'Boliche':'Cachorro','Bolbusters':'2025'}
+  info = {'Equipo':'Bolbusters','año de la participacion':'2025'}
   print(info[nom])
      #KeyError: No se ejecuta el print porque no existe la llave nom en el diccionario info.
 
@@ -40,8 +40,8 @@ print("Ejercicios de Nivel 1:")
      #ValueError: No se ejecuta el print porque no se puede convertir un string a un int.
 
 # Ejemplo 9, ImportError:
-  import win 1
-  import win 2
-     #ImportError: No se ejecuta el import porque no existe el modulo win 1 y win 2.
+  import math 1
+  import math 2
+     #ImportError: No se ejecuta el import porque no existe el modulo math 1 y math 2.
 
 #END OF DAY-15
