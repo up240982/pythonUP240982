09fd8adaa4ca00a91bd058ab102332c24940ce4b
UP240982 <up240982@alumnos.>
2025-04-03 20:20:27 -0600

    listo, pongame 10 porfa!!!

---
+++ 15_Day/day-15.py
@@ -0,0 +1,47 @@
+##Ejercicios Nivel 1
+print("Ejercicios de Nivel 1:")
+
+ print("Todo esto marca error pero asi es jsjsjs :")
+ print("Ejemplos de errores:")
+ 
+# Ejemplo 1, Syntaxerror
+ print('Hoy gana el America'
+       #SyntaxError: No se ejecuta el print porque no se cierra el parentesis.
+ 
+# Ejemplo 2, NameError     
+  print(ya me caburri, bueno desde que comenzo la programacion)
+     #NameError: No se ejecuta el print porque no se ha definido la variable ya me caburri, bueno desde que comenzo la programacion.
+ 
+# Ejemplo 3, IndexError
+  num = [6,7,10,17,77,11,29]
+  print(num[0])
+  #IndexError: No se ejecuta el print porque no existe el indice ? en la lista num.
+
+# Ejemplo 4, ModuleNotFoundError:
+  import errupstorpedo
+     #ModuleNotFoundError: No se ejecuta el import porque no existe el modulo errupstorpedo.
+
+# Ejemplo 5, AttributeError
+  import date
+  date.ends
+     #AttributeError: No se ejecuta el import porque no existe el modulo date.
+
+# Ejemplo 6, KeyError:
+  info = {'Boliche':'Cachorro','Bolbusters':'2025'}
+  print(info[nom])
+     #KeyError: No se ejecuta el print porque no existe la llave nom en el diccionario info.
+
+# Ejemplo 7, TypeError:
+  print('CR' + 7)
+     #TypeError: No se ejecuta el print porque no se puede sumar un string con un int.
+ 
+# Ejemplo 8, ValueError:
+  print int('Ya me aburri profe')
+     #ValueError: No se ejecuta el print porque no se puede convertir un string a un int.
+
+# Ejemplo 9, ImportError:
+  import win 1
+  import win 2
+     #ImportError: No se ejecuta el import porque no existe el modulo win 1 y win 2.
+
+#END OF DAY-15
